5f6e5bae61ea5a48e946beabf4291bd97f3e1f4e
duonggiakhong <20211tt2616@mai.tdc.edu.vn>
2023-06-12 23:57:34 +0700

    v7 crud user

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,4 +1,5 @@
-import { json } from "body-parser";
+// import { json } from "body-parser";
+//connect dataBase
 import pool from "../configs/connectDB";
 
 let getHomePage = async (req, res) => {
@@ -18,11 +19,35 @@ let getDetailPage = async (req, res) => {
     let user = await pool.execute('SELECT * FROM users WHERE id = ?', [id]);
     return res.send(JSON.stringify(user[0]));
 }
+
+//create user new 
 let CreateNewuser = async (req, res) => {
 
     let { firstName, lastName, email, address } = req.body;
     await pool.execute('INSERT INTO users (firstName, lastName, email, address) values (?,?,?,?)', [firstName, lastName, email, address]);
-    return res.render('/');
+    //
+    return res.redirect('/');
+}
+
+//delete user
+let deleteUser = async (req, res) => {
+    let userId = req.body.userId;
+    await pool.execute('DELETE FROM users WHERE id = ?', [userId]);
+    return res.redirect('/');
+}
+
+//edit user
+let editUser = async (req, res) => {
+    let id = req.params.id;
+    let [user] = await pool.execute('SELECT * FROM users WHERE id = ?', [id]);
+    return res.render('update.ejs', { dataUser: user[0] });
+}
+
+//update user 
+let updateUserPage = async (req, res) => {
+    let { firstName, lastName, email, address, id } = req.body;
+    await pool.execute('UPDATE users SET firstName = ?, lastName= ?, email = ?, address = ? WHERE id = ?', [firstName, lastName, email, address, id]);
+    return res.redirect('/');
 }
 
 module.exports = {
@@ -30,4 +55,9 @@ module.exports = {
     getAboutPage,
     getDetailPage,
     CreateNewuser,
+    deleteUser,
+    editUser,
+    updateUserPage,
+
+
 }
--- src/route/web.js
+++ src/route/web.js
@@ -5,8 +5,14 @@ const initwebRouter = (app) => {
     router.get('/about', homeController.getAboutPage)
     router.get('/detail/user/:id', homeController.getDetailPage);
     router.get('/', homeController.getHomePage);
+    //create user new
     router.post('/create-new-user', homeController.CreateNewuser);
-
+    //delete user 
+    router.post('/delete-user', homeController.deleteUser);
+    //edit-user
+    router.get('/edit-user/:id', homeController.editUser);
+    //update user
+    router.post('/update-user', homeController.updateUserPage);
     return app.use('/', router);
 }
 
